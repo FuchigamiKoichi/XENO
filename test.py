661de0d51c904b40b22f9ec5c7a676657189b976
hitori51 <e235714@ie.u-ryukyu.ac.jp>
2025-04-06 21:44:02 +0900

    兵士の追加

--- test.py
+++ test.py
@@ -52,6 +52,17 @@ class Card:
         self.name = name
         self.player = player
         self.field = field
+    
+    def move(self):
+        field = self.field
+        for i in range(len(self.player.hands)):
+            if type(self) == type(self.player.hands[i]):
+                self.player.hands.pop(i)
+                break
+        field.played.append(self)
+        
+
+
 
 # カード1:少年
 # 一枚目では効果無し、二枚目では「公開処刑」
@@ -83,9 +94,36 @@ class Card1(Card):
             trushNumber = int(input(msg))
             opponent.hands.pop(trushNumber)
         
-        field.played.append(self)
-            
+        super().move()
 
+
+# カード2:兵士
+class Card2(Card):
+    def __init__(self, field, player):
+        super().__init__(number=2, name='兵士', field=field, player=player)
+    
+    def play(self):
+        field = self.field
+        print(self.player.name,'が',self.name,'を使用しました。')
+        msg = '相手を選択してください'+'\n'
+        opponentPlayers = field.players
+        opponentPlayers.remove(self.player)
+        for i in range(len(opponentPlayers)):
+            player = opponentPlayers[i]
+            msg += str(i) + '：' + player.name + '\n'
+                
+        opponentNumber = int(input(msg))
+        opponent = field.players[opponentNumber]
+        cards = [Card1(field=self.field,player=self),Card2(field=self.field,player=self)]
+        msg = opponent.name+'が持っていそうなカードを予想してください。\n'
+        for card in cards:
+            msg += str(card.number)+'：'+card.name+'\n'
+        predNumber = int(input(msg))
+        predCard = cards[predNumber-1]
+        if inType(type=type(predCard),list=opponent.hands):
+            opponent.live = False
+        
+        super().move()
 
 
 # プレイヤーを作成
@@ -95,11 +133,12 @@ player2 = Player("Bob")
 Game = Field(players=[player1,player2])
 card1_1 = Card1(field=Game,player=player1)
 card1_2 = Card1(field=Game,player=player1)
+card2_1 = Card2(field=Game,player=player1)
 card1_3 = Card1(field=Game,player=player2)
 card1_4 = Card1(field=Game,player=player2)
 
 
-player1.hands = [card1_1,card1_2]
+player1.hands = [card1_1,card2_1]
 player2.hands = [card1_3,card1_4]
 
 # テスト
@@ -118,8 +157,8 @@ for card in Game.played:
 print('場に出ているカード','：',cards)
 print()
 
-card1_1.play()
-card1_2.play()
+card2_1.play()
+print()
 
 for player in Game.players:
     cards = []
@@ -131,3 +170,6 @@ cards = []
 for card in Game.played:
     cards.append(card.name)
 print('場に出ているカード','：',cards)
+print()
+print(player1.name,'の命は',player1.live,'です。')
+print(player2.name,'の命は',player2.live,'です。')
