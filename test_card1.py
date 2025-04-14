8769865815d8455c2173d58f5f1fbe61740d4b4e
hitori51 <e235714@ie.u-ryukyu.ac.jp>
2025-04-14 18:11:09 +0900

    ゲームの開始から終了までの実装

--- test_card1.py
+++ test_card1.py
@@ -3,14 +3,23 @@ import module as m
 def choice(now,choices,kind):
     return choices[0]
 
+def test_opponent_choice():
+    p = m.Player(name='test_player')
+    o = m.Player(name='test_object')
+    f = m.Field(players=[p,o])
+    card = m.Card1(player=p,field=f)
+    p.hands.append(card)
+    opponent = p.hands[0].opponentChoice(choice=choice)
+    assert opponent != None
+
 def test_number_player():
     p = m.Player(name='test_player')
     o = m.Player(name='test_object')
     f = m.Field(players=[p,o])
-    f.draw(player=p,choice=choice)
-    f.draw(player=o,choice=choice)
-    card = m.Card1(player=p,field=f)
-    p.hands.append(card)
+    card1 = m.Card1(player=p,field=f)
+    card2 = m.Card1(player=p,field=f)
+    p.hands.append(card1)
+    p.hands.append(card2)
     p.hands[0].play(choice=choice)
     assert len(p.hands) == 1
 
@@ -18,31 +27,57 @@ def test_number_object():
     p = m.Player(name='test_player')
     o = m.Player(name='test_object')
     f = m.Field(players=[p,o])
-    f.draw(player=p,choice=choice)
+    card1 = m.Card1(player=p,field=f)
+    card2 = m.Card1(player=p,field=f)
     f.draw(player=o,choice=choice)
-    card = m.Card1(player=p,field=f)
-    p.hands.append(card)
+    p.hands.append(card1)
+    p.hands.append(card2)
     p.hands[0].play(choice=choice)
     assert len(o.hands) == 1
 
-def test_first_time():
-    p = m.Player(name='test_player')
-    o = m.Player(name='test_object')
-    f = m.Field(players=[p,o])
-    f.draw(player=p,choice=choice)
-    f.draw(player=o,choice=choice)
-    card = m.Card1(player=p,field=f)
-    p.hands.append(card)
-    p.hands[0].play(choice=choice)
-    assert len(f.played) == 1
 
-def test_second_time():
+def test_played_none():
     p = m.Player(name='test_player')
     o = m.Player(name='test_object')
     f = m.Field(players=[p,o])
-    f.draw(player=p,choice=choice)
-    f.draw(player=o,choice=choice)
+    card = m.Card1(player=p,field=f)
+    p.hands.append(card)
+    assert len(f.played) == 0
+
+def test_p_hands():
+    p = m.Player(name='test_player')
+    o = m.Player(name='test_object')
+    f = m.Field(players=[p,o])
+    card = m.Card1(player=p,field=f)
+    p.hands.append(card)
+    assert type(p.hands[0]) == m.Card1
+
+def test_move():
+    p = m.Player(name='test_player')
+    o = m.Player(name='test_object')
+    f = m.Field(players=[p,o])
+    card = m.Card1(player=p,field=f)
+    p.hands.append(card)
+    assert type(p.hands[0].move()) == m.Card1
+
+def test_played():
+    p = m.Player(name='test_player')
+    o = m.Player(name='test_object')
+    f = m.Field(players=[p,o])
     card = m.Card1(player=p,field=f)
     p.hands.append(card)
     p.hands[0].play(choice=choice)
-    assert len(f.hands) == 1
+    if m.inType(type=m.Card1,list=f.played):
+        result = 'contain'
+    else:
+        result = 'omit'
+    assert result == 'contain'
+
+def test_holder():
+    p = m.Player(name='test_player')
+    o = m.Player(name='test_object')
+    f = m.Field(players=[p,o])
+    card1_1 = m.Card1(player=p,field=f)
+    card1_2 = m.Card2(player=o,field=f)
+    p.hands.append(card)
+    p.hands[0].play(choice=choice)
