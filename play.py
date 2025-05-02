e1172b95c6f1d7f94229218e7fd469e11c9472fe
hitori51 <e235714@ie.u-ryukyu.ac.jp>
2025-05-02 15:05:00 +0900

    改善したコードの簡単なバグとり終了

--- play.py
+++ play.py
@@ -1,49 +1,55 @@
-import xeno as x
+import xeno_module as x
 import cpu
 import csv
+import random
 
 # cpuの行動を定義
 def choice_cpu(now,choices,kind):
     # cpu.pyでAIを読み込んで選択肢を検討
-    return cpu.cpu(now=now,choices=choices,kind=kind)
+    number = random.randint(0,len(choices)-1)
+    if kind=='opponentChoice':
+        choice = choices[number]
+        result = choice['select_number']
+    else:
+        result = choices[number]
+    return result
 
 # cpuの命名規則を定義
 def get_name_cpu(index:int):
     return 'test_player'+str(index)
 
 
-# プレイヤーの行動を定義
-def choice_player(now,choices,kind):
-    # input関数でプレイヤーに選択させる
-    return input(f'現在状況：{now}, モード：{kind}, 選択肢：{choices}')
+# # プレイヤーの行動を定義
+# def choice_player(now,choices,kind):
+#     # input関数でプレイヤーに選択させる
+#     return input(f'現在状況：{now}, モード：{kind}, 選択肢：{choices}')
 
-def get_name_player(index:int):
-    # input関数でプレイヤーに名前を選択させる
-    return input(f'名前を入力してください')
+# def get_name_player(index:int):
+#     # input関数でプレイヤーに名前を選択させる
+#     return input(f'名前を入力してください')
 
-game = x.Game(2, get_name_funcs=[get_name_cpu,get_name_player])
-data = game.game(choice_funcs=[choice_cpu,choice_player])
+# game = x.Game(2, get_name_funcs=[get_name_cpu,get_name_player])
+# data = game.game(choice_funcs=[choice_cpu,choice_player])
 
 
 # while True:
-# # for _ in range(10**4):
-#     game = x.Game(2, get_name_funcs=[get_name_cpu,])
-#     data = game.game(choice_func=choice)
-#     if data[0]:
-#         log = data[1]
-#         if log[0][-1] == log[1][-1] == 'win':
-#             print('err：勝者が複数人います')
-#         with open('result/log.csv', 'a') as f:
-#         # with open('result/log.csv', 'a') as f:
-#             writer = csv.writer(f)
-#             for l in log:
-#                 writer.writerow(l)
-#     else:
-#         log = data[2]
-#         err = data[1]
-#         print(err)
-#         with open('result/log.csv', 'a') as f:
-#             writer = csv.writer(f)
-#             for l in log:
-#                 writer.writerow(l)
-#         break
+for _ in range(10**5):
+    game = x.Game(2, get_name_funcs=[get_name_cpu,get_name_cpu])
+    data = game.game(choice_funcs=[choice_cpu,choice_cpu])
+    if data[0]:
+        log = data[1]
+        if log[0][-1] == log[1][-1] == 'win':
+            print('err：勝者が複数人います')
+        with open('result/log.csv', 'a') as f:
+            writer = csv.writer(f)
+            for l in log:
+                writer.writerow(l)
+    else:
+        log = data[2]
+        err = data[1]
+        print(err)
+        with open('result/log.csv', 'a') as f:
+            writer = csv.writer(f)
+            for l in log:
+                writer.writerow(l)
+        break
